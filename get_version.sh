69a8561341989478c19ea3160bae2b3ba428f535
ReFil <31960031+ReFil@users.noreply.github.com>
2024-11-16 18:24:26 +0000

    Add more advanced versioning capabilities

--- get_version.sh
+++ get_version.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Get the date, first 4 chars of branch name and short commit hash
+# Get the date, layout name and short commit hash
 date=$(date -u +"%Y%m%d")
-branch=${1:-$(git rev-parse --abbrev-ref HEAD | cut -c1-4)}
-commit=${2:-$(git rev-parse --short HEAD)}
+layout=$1
+module_commit=$(git --git-dir=/tmp/zmk-config/MWave-zephyr-module/.git/ --work-tree=/tmp/zmk-config/MWave-zephyr-module rev-parse --short HEAD)
+commit=$(git rev-parse --short HEAD)
 
 uppercase_char() {
     local char=$1
@@ -21,6 +22,8 @@ transform_char() {
         echo "<&kp N${char}>, "
     elif [ "$char" = "." ]; then
         echo "<&kp DOT>, "
+    elif [ "$char" = "-" ]; then
+        echo "<&kp MINUS>, "
     fi
 }
 
@@ -30,17 +33,26 @@ for ((i = 0; i < ${#date}; i++)); do
     formatted_date+=$(transform_char "${date:$i:1}")
 done
 
-# Insert separator between date and branch
+# Insert separator between date and layout
 formatted_date+="<&kp MINUS>, "
 
-# Iterate over the branch and format characters
-formatted_branch=""
-for ((i = 0; i < ${#branch}; i++)); do
-    formatted_branch+=$(transform_char "${branch:$i:1}")
+# Iterate over the layout and format characters
+formatted_layout=""
+for ((i = 0; i < ${#layout}; i++)); do
+    formatted_layout+=$(transform_char "${layout:$i:1}")
 done
 
-# Insert separator between branch and commit hash
-formatted_branch+="<&kp MINUS>, "
+# Insert separator between layout and commit hash
+formatted_layout+="<&kp MINUS>, "
+
+# Iterate over the module commit and format characters
+formatted_module_commit=""
+for ((i = 0; i < ${#layout}; i++)); do
+    formatted_module_commit+=$(transform_char "${module_commit:$i:1}")
+done
+
+# Insert separator between module commit and commit hash
+formatted_module_commit+="<&kp MINUS>, "
 
 # Iterate over the commit hash and format characters
 formatted_commit=""
@@ -49,15 +61,15 @@ for ((i = 0; i < ${#commit}; i++)); do
 done
 
 # Combine the formatted string, add trailing carriage return
-formatted_result="$formatted_date$formatted_branch$formatted_commit"
+formatted_result="$formatted_date$formatted_layout$formatted_module_commit$formatted_commit"
 formatted_result+="<&kp RET>"
 
 echo $formatted_result
 # Create new macro to define version, overwrite previous one
 
-echo '#define VERSION_MACRO' > "config/version.dtsi"
-echo 'macro_ver: macro_ver {' >> "config/version.dtsi"
-echo 'compatible = "zmk,behavior-macro";' >> "config/version.dtsi"
-echo '#binding-cells = <0>;' >> "config/version.dtsi"
-echo "bindings = $formatted_result;" >> "config/version.dtsi"
-echo '};' >> "config/version.dtsi"
+echo '#define VERSION_MACRO' > "$2/config/version.dtsi"
+echo 'macro_ver: macro_ver {' >> "$2/config/version.dtsi"
+echo 'compatible = "zmk,behavior-macro";' >> "$2/config/version.dtsi"
+echo '#binding-cells = <0>;' >> "$2/config/version.dtsi"
+echo "bindings = $formatted_result;" >> "$2/config/version.dtsi"
+echo '};' >> "$2/config/version.dtsi"
